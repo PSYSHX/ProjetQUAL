aeb7ad969eda2c7d99d4697e6365a483cf404beb
PSYSHX <emforaccsxxx@gmail.com>
2024-10-22 11:56:45 +0200

    TP1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import isen.library.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Créer un objet Person
+        Person person = new Person();
+        System.out.println("Nom : " + person.getName());
+        System.out.println("Âge : " + person.getYears());
+
+        // Modifier les valeurs de l'objet
+        person.setName("John");
+        person.setYears(30);
+        System.out.println("Nouveau nom : " + person.getName());
+        System.out.println("Nouvel âge : " + person.getYears());
     }
 }
